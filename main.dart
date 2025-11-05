35919f7bfc662ad682ac39d35a4776c0a4db9a89
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-05 15:26:23 +0100

    main

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age = 30;
+  print(age);
+
+  String name = "chun-li";
+  // name = 30; // won't work
+  name = "ken";
+  print(name);
+
+  bool isNight = false;
+  print(isNight);
+}
