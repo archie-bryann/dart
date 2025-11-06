51519f93e0522896ce3829f82891afae9b060d54
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-06 09:01:59 +0100

    classes

--- 4_classes.dart
+++ 4_classes.dart
@@ -1,11 +1,20 @@
 void main() {
-  User userOne = User();
-  userOne.login();
+  User userOne = User('luigi', 25);
+  print(userOne.username);
+
+  User userTwo = User('mario', 30);
+  print(userTwo.username);
 }
 
 class User {
-  String username = 'mario';
-  int age = 25;
+  // String username = 'mario';
+  // int age = 25;
+
+  String username;
+  int age;
+
+  // constructor
+  User(this.username, this.age);
 
   void login() {
     print('$username logged in');
